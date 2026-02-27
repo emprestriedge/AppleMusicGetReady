657270101589c5c651373a2bc9c3dd03f10567e2
emprestriedge <gkrgsqn29y@privaterelay.appleid.com>
2026-02-26 21:14:20 -0600

    Remove native webview injection code from AppDelegate

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -9,22 +9,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        // Inject a flag into the webview so JS knows it's running natively
-        let userScript = WKUserScript(
-            source: "window.__CAPACITOR_NATIVE__ = true;",
-            injectionTime: .atDocumentStart,
-            forMainFrameOnly: false
-        )
-        
-        // Find the webview and inject
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-            if let webView = self.window?.rootViewController?.view.subviews
-                .compactMap({ $0 as? WKWebView }).first {
-                webView.configuration.userContentController.addUserScript(userScript)
-            }
-        }
-        
         return true
     }
 
